65c7cb40b9105eaf987cebfa6ee06ad6efae8c94
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-11 17:37:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,9 @@ def batch_badge_creator(name_array)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index |name, index|
-    "Hello #{name}! You'll be assigned to room #{index + 1}!" if index < 6
+  attendees.each do |name|
+    room = attendees.index(name)
+    room += 1
+    "Hello #{name}! You'll be assigned to room #{room}!"
   end
 end
