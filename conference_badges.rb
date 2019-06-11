e3a4748a30110ccab9941fa46d054d90e5e69569
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-11 17:26:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,5 +13,7 @@ def batch_badge_creator(name_array)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index { |name, index| return "Hello #{name}! You'll be assigned to room #{index}!" }
+  attendees.each_with_index { |name, index| return "Hello #{name}! You'll be assigned to room #{index + 1}!" }
 end
+
+attendees = "able", "frank", "george",]
